8b8b6b7c6bb045e91d9c8ae5ab28a640407c5196
Vijayababu Venugopal <vijaybabu27@gmail.com>
2019-06-14 19:53:12 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+const hbs = require('hbs');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const forexapi = require('./getForex');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', {
+      pageTitle: 'Home'
+  })
+});
+
+app.get('/about.html', (req, res) => {
+  res.render('about.hbs', {
+      pageTitle: 'About'
+  })
+});
+
+app.post('/getForex.html', (req, res) => {
+  console.log(req.body);
+  var input = req.body;
+  setTimeout(function() {
+    forexapi.getForexRates(input.baseSymbol, input.toSymbol, (errorMessage, results) => {
+      if (errorMessage) {
+          res.render('error.hbs', errorMessage);
+      } else {
+        res.render('results.hbs', results);
+        console.log(`It's currently 1 ${results.baseCur} is equal to ${results.rates} ${results.toCur}`);
+      }
+    });
+  }, 1000);
+});
+
+app.listen(3000, () => {
+  console.log('Server running at port # 3000.....');
+})
---
+++ getForex.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+var getForexRates = (baseCur, toCur, callback) => {
+    
+    request({
+        url: `https://api.exchangeratesapi.io/latest?base=${baseCur}&symbols=${toCur}`,
+        json: true
+    }, (error, response, body) => {
+        console.log(response.statusCode);
+        console.log(body.status);
+        if (response.statusCode === 403) {
+            callback(JSON.parse('{"error": "Unable to connect to exchange rates API server."}'), null);
+        } else if (response.statusCode === 400) {
+            callback('Unable to fetch forex rates.', null);
+        } else if (response.statusCode === 200) {
+            console.log('Inside 200 = ' + body);
+            callback(undefined, {
+                baseCur: baseCur,
+                toCur: toCur,
+                rates: body.rates.toCur
+            });
+        }
+    });
+
+};
+
+module.exports.getForexRates = getForexRates;
